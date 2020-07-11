7dbff5574edfb5042bb2564a9e1c32f465f49ea9
Leda Duelo <leda.duelo@gmail.com>
2020-07-11 19:07:03 +0200

    clean bug on tokenize

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -58,125 +58,6 @@ def load_data(database_filepath):
 
 
 
-def tokenize2(text):
-    """
-    function for tokenize 
-    Arguments:
-    ----
-        List of messages 
-    Output:
-    ----
-        Clean and tokenize text for the model
-    """
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    
-    detected_urls = re.findall(url_regex, text)
-    for url in detected_urls:
-        text = text.replace(url, "urlplaceholder")
-    
-    # tokenize text and lemma
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
-
-
-
-def tokenize2(text):
-    """
-    function for tokenize  with stop words
-    Arguments:
-    ----
-        List of messages 
-    Output:
-    ----
-        Clean and tokenize text for the model 
-    """
-    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
-    
-    # tokenize text
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()    
-    #stemming
-    tokens_stem = [PorterStemmer().stem(t) for t in tokens]
-
-    # lemmatize and remove stop words
-    stop_words = stopwords.words("english")
-    clean_tokens = [lemmatizer.lemmatize(word) for word in tokens_stem if word not in stop_words]
-
-    return clean_tokens
-
-
-
-class StartingVerbExtractor2(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-def build_model2():
-    """
-    Build model with pipleline complex and starting verb and Grid to check parameters
-    """
-    # create pipeline
-    pipeline = Pipeline([
-        ('features', FeatureUnion([
-
-            ('text_pipeline', Pipeline([
-                ('vect', CountVectorizer(tokenizer=tokenize)),
-                ('tfidf', TfidfTransformer())
-            ])),
-
-            ('starting_verb', StartingVerbExtractor())
-        ])),
-
-        ('clf', MultiOutputClassifier(RandomForestClassifier()))
-    ])
-
-
-    # use GridSearch to tune model with optimal parameters
-    parameters = { 'vect__max_df': (0.75, 1.0),
-                'clf__estimator__n_estimators': [10, 20],
-                'clf__estimator__min_samples_split': [2, 5]
-             }
-    #model = GridSearchCV (pipeline, param_grid= parameters, verbose =7 )
-    model = pipeline
-    return model
-
-def build_model2():
-    """
-    Build model with a pipeline simple with parameters
-    """
-    # create pipeline
-    pipeline = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf', MultiOutputClassifier(RandomForestClassifier()))])
-
-    # use GridSearch to tune model with optimal parameters
-    parameters = { 'vect__max_df': (0.75, 1.0),
-                'clf__estimator__n_estimators': [10, 20],
-                'clf__estimator__min_samples_split': [2, 5]
-             }
-    model = GridSearchCV (pipeline, param_grid= parameters, verbose =7 )
-    #model = pipeline
-    return model
 def tokenize(text):
     '''
     Tokenize function simple 
@@ -194,11 +75,9 @@ def tokenize(text):
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(tok)
+        clean_tokens.append(clean_tok)
 
     return clean_tokens
-
-
 
 
 def build_model():
